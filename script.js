4e0fa8b891c5b9bccb44081deb7b9654fb3ebace
Sahil Thombare <122919311+sahil0101today@users.noreply.github.com>
2024-08-08 17:50:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+function showAlert(alertId) {
+    var alertElement = document.getElementById(alertId);
+    alertElement.style.display = 'block';
+    setTimeout(function() {
+        alertElement.style.display = 'none';
+    }, 3000);
+}
+
+document.getElementById('submitForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var url = document.getElementById('url').value;
+    var recipients = document.getElementById('recipients').value;
+    var email = sessionStorage.getItem('email');
+    var password = sessionStorage.getItem('password');
+
+    // Replace this with your GitHub repository details
+    var githubRepo = 'sahil0101today/MOE-Reports';
+    var githubToken = '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';
+
+    // Create a GitHub API URL to trigger the workflow dispatch
+    var apiUrl = `https://api.github.com/repos/${githubRepo}/actions/workflows/MOE_reports.yml/dispatches`;
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Authorization': `token ${githubToken}`,
+            'Accept': 'application/vnd.github.v3+json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            ref: 'main', // The branch you want to run the workflow on
+            inputs: {
+                url: url,
+                recipients: recipients,
+                email: email,
+                password: password
+            }
+        })
+    }).then(response => {
+        if(response.ok) {
+            showAlert('successAlert');
+            document.getElementById('submitForm').reset();
+        } else {
+            response.json().then(data => {
+                console.error('Error:', data);
+                showAlert('errorAlert');
+            });
+        }
+    }).catch(error => {
+        console.error('Error:', error);
+        showAlert('errorAlert');
+    });
+});
